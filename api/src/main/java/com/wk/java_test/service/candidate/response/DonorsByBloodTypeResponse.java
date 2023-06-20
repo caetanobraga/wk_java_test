93331dd876c7caa62685ac346debff9e1c0c3510
caetanobraga <caetanobraga@gmail.com>
2023-06-20 00:52:50 -0300

    feature/ageMediaByBloodType

---
+++ api/src/main/java/com/wk/java_test/service/candidate/response/DonorsByBloodTypeResponse.java
@@ -0,0 +1,11 @@
+package com.wk.java_test.service.candidate.response;
+
+public class DonorsByBloodTypeResponse {
+    private String bloodType;
+    private Long donorCount;
+
+    public DonorsByBloodTypeResponse(String bloodType, Long donorCount){
+        this.bloodType = bloodType;
+        this.donorCount = donorCount;
+    }
+}
