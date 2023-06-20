43a27ece91898c66142aa10b4b9e6bd2dd779ddc
caetano braga <caetanobraga@gmail.com>
2023-06-20 16:07:26 -0300

    adding candidates by state graph

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,74 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wk-app';
+  @ViewChild('fileInput') fileInput: any;
+  @ViewChild('canvas1', { static: true }) elemento1: ElementRef;
+
+
+  constructor(private http: HttpClient) {
+    this.elemento1 = new ElementRef(null);
+  }
+
+  inputFileChange(event: any) {
+    const file = event.target.files[0];
+  if (file) {
+    const formData = new FormData();
+      formData.append('file', file);
+
+      const apiUrl = 'http://localhost:8082/api/v1/candidates';
+
+      this.http.post(apiUrl, formData)
+      .subscribe(
+        response => {
+          console.log('Arquivo enviado com sucesso', response);
+          this.getCandidatesByState();
+        },
+        error => {
+          console.error('Erro ao enviar o arquivo', error);
+        }
+      );
+    };
+  }
+
+  getCandidatesByState(){
+    const apiUrl = 'http://localhost:8082/api/v1/candidates';
+
+    this.http.get(apiUrl).subscribe({
+      next: (response: any) => {
+        this.processChartData(response);
+      },
+      error: error => {
+        console.error('Erro ao obter os dados da API', error);
+      }
+    });
+  }
+
+  processChartData(data: { state: string, candidatesNumber: number }[]){
+    console.log(data);
+
+    const labels: string[] = data.map( item => item.state);
+
+    const values: number[] = data.map(item => item.candidatesNumber);
+
+    console.log(labels);
+    console.log(values);
+
+    const chartType = "bar";
+    new Chart(this.elemento1.nativeElement, {
+      type: chartType,
+      data: {
+        labels: labels,
+        datasets:[
+          {
+            label: "Número de candidatos por UF",
+            data: values,
+          }
+        ]
+      },
+    });
+  }
+
+
+
+
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
