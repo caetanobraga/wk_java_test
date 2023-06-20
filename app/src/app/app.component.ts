fdf05fdbcc1c00dbc8fe7f9f6e98385a5f26358e
caetano braga <caetanobraga@gmail.com>
2023-06-20 16:28:13 -0300

    imc media by age graphic added

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -10,11 +10,15 @@ import Chart from 'chart.js/auto';
 export class AppComponent {
   title = 'wk-app';
   @ViewChild('fileInput') fileInput: any;
-  @ViewChild('canvas1', { static: true }) elemento1: ElementRef;
+  @ViewChild('canvas1', { static: true }) element1: ElementRef;
+  @ViewChild('canvas2', { static: true }) element2: ElementRef;
 
 
   constructor(private http: HttpClient) {
-    this.elemento1 = new ElementRef(null);
+    this.element1 = new ElementRef(null);
+    this.element2 = new ElementRef(null);
+
+    
   }
 
   inputFileChange(event: any) {
@@ -30,6 +34,7 @@ export class AppComponent {
         response => {
           console.log('Arquivo enviado com sucesso', response);
           this.getCandidatesByState();
+          this.getImcByAgeGroup();
         },
         error => {
           console.error('Erro ao enviar o arquivo', error);
@@ -43,7 +48,7 @@ export class AppComponent {
 
     this.http.get(apiUrl).subscribe({
       next: (response: any) => {
-        this.processChartData(response);
+        this.processChartCandidatesByStateData(response);
       },
       error: error => {
         console.error('Erro ao obter os dados da API', error);
@@ -51,18 +56,27 @@ export class AppComponent {
     });
   }
 
-  processChartData(data: { state: string, candidatesNumber: number }[]){
-    console.log(data);
+  getImcByAgeGroup(){
+    const apiUrl = 'http://localhost:8082/api/v1/media-imc';
+
+    this.http.get(apiUrl).subscribe({
+      next: (response: any) => {
+        this.processChartImcByAgeGroup(response);
+      },
+      error: error => {
+        console.error('Erro ao obter os dados da API', error);
+      }
+    });
+  }
+
+  processChartCandidatesByStateData(data: { state: string, candidatesNumber: number }[]){
 
     const labels: string[] = data.map( item => item.state);
 
     const values: number[] = data.map(item => item.candidatesNumber);
 
-    console.log(labels);
-    console.log(values);
-
     const chartType = "bar";
-    new Chart(this.elemento1.nativeElement, {
+    new Chart(this.element1.nativeElement, {
       type: chartType,
       data: {
         labels: labels,
@@ -76,7 +90,27 @@ export class AppComponent {
     });
   }
 
+  processChartImcByAgeGroup(data: { ageGroup: number, imcMedia: number }[]){
 
+    const labels: string[] = data.map( item => item.ageGroup+1 + " - " + (item.ageGroup+10));
 
+    const values: number[] = data.map(item => item.imcMedia);
 
+    console.log(labels);
+    console.log(values);
+
+    const chartType = "bar";
+    new Chart(this.element2.nativeElement, {
+      type: chartType,
+      data: {
+        labels: labels,
+        datasets:[
+          {
+            label: "Imc médio por faixa etária",
+            data: values,
+          }
+        ]
+      },
+    });
+  }
 }
